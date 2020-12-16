675c75ec18634510d6844ccb999d48ad45125a26
Narith Seang <nseang2@gmail.com>
2020-12-15 20:46:42 -0500

    initial refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './modals/menu-item.modal';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+  menuItem: MenuItem = {
+    name: 'Beef Stew',
+    description: 'Carrots, potato, beef stew on white rice',
+    imagePath: 'assets/pics/riceBeefStew.jpg'
+  }
+  menuItems: MenuItem[] = [
+    {
+      name: 'Beef Stew',
+      description: 'Carrots, potato, beef stew on white rice',
+      imagePath: 'assets/pics/riceBeefStew.jpg'
+    },
+    {
+      name: 'Curry Fish',
+      description: 'Carrots, potato, beef broth with Fried Tempura Fish on Rice',
+      imagePath: 'assets/pics/curryFish.jpg'
+    },
+    {
+      name: 'Jasmine Rice Special',
+      description: 'Jasmine rice with steam meatloaf, grilled pork, fried eggs, shredded pork',
+      imagePath: 'assets/pics/comTham.jpg'
+    },
+    {
+      name: '',
+      description: '',
+      imagePath: ''
+    },
+    {
+      name: '',
+      description: '',
+      imagePath: ''
+    },
+    {
+      name: '',
+      description: '',
+      imagePath: ''
+    },
+    {
+      name: '',
+      description: '',
+      imagePath: ''
+    },
+
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppMenuItem } from './menu-item/menu-item.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMenuItem
   ],
   imports: [
     BrowserModule,
