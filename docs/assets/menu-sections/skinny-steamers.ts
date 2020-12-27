f9397df4edf84ee13a63b6a615b223890e141455
Narith Seang <nseang2@gmail.com>
2020-12-27 00:42:40 -0500

    github pages commit

---
+++ docs/assets/menu-sections/skinny-steamers.ts
@@ -0,0 +1,70 @@
+import { Menu } from 'src/app/modals/menu.modal'
+
+export let skinnySteamersMenu: Menu = {
+  'section': {
+    'sectionName': 'Skinny Steamers',
+    'sectionDescription': 'Your side choice of Fried Rice, White Rice, or Lo Mein /n/' +
+    'with brown rice-add $.50 or hibachi rice-add $1 /n/ ' +
+    'All Skinny Steamer entrees served with - /n/ ' + 
+    'celery, carrot, broccoli & zucchini. /n/ ' +
+    'This dish does not contain sauce /n/ ' + 
+    '(Sauce on side is an additional cost)',
+    'menuItems': [
+      {
+        'title': 'Chicken and Vegetable Steamer',
+        'subTitle': null,
+        'description': '279Cal - 6.2g Fat - 0g Carb - 42.7g Protein',
+        'imagePath': null,
+        'price': null,
+        'priceSM': 9,
+        'priceLG': 12
+      },
+      {
+        'title': 'Flank Steak and Vegetable Steamer',
+        'subTitle': null,
+        'description': '330Cal - 14.1g Fat - 0g Carb - 42.7g Protein',
+        'imagePath': null,
+        'price': null,
+        'priceSM': 11,
+        'priceLG': 14
+      },
+      {
+        'title': 'Shrimp and Vegetable Steamer',
+        'subTitle': null,
+        'description': '303Cal - 4.2g Fat - 0g Carb - 58.2g Protein',
+        'imagePath': null,
+        'price': null,
+        'priceSM': 10,
+        'priceLG': 13
+      },
+      {
+        'title': 'Tofu and Vegetable Steamer',
+        'subTitle': null,
+        'description': '88Cal - 5.3g Fat - 2.1g Carb - 10.3g Protein',
+        'imagePath': null,
+        'price': null,
+        'priceSM': 9,
+        'priceLG': 12
+      },
+      {
+        'title': 'Vegetable Steamer',
+        'subTitle': null,
+        'description': '125Cal - 0g Fat - 30g Carb - 10g Protein',
+        'imagePath': null,
+        'price': null,
+        'priceSM': 9,
+        'priceLG': 12
+      },
+      {
+        'title': 'House Steamer',
+        'subTitle': null,
+        'description': 'Choose 3 options above to complete this combination entree. (duplicates allowed) Tofu add $2',
+        'imagePath': null,
+        'price': null,
+        'priceSM': 12,
+        'priceLG': 15
+      },
+      
+    ]
+  }
+}
